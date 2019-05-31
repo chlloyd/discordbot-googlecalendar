13b5781f71f6dd17f913f800f93e4a47d29e6484
chlloyd <christopher.lloyd3@mail.bcu.ac.uk>
2019-05-31 17:03:20 +0100

    Formatting Datetime from the Calendar API response

--- googlecalendar.py
+++ googlecalendar.py
@@ -9,15 +9,11 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
-def main():
+def initiate():
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -25,33 +21,45 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server()
-        # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
+    return service
 
+def getrecentevents(results):
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='bolkubtob5hn0q1i9btftud944@group.calendar.google.com', timeMin=now,
-                                          maxResults=10, singleEvents=True,
+    print('Getting the upcoming %i events' % results)
+    events_result = initiate().events().list(calendarId='bolkubtob5hn0q1i9btftud944@group.calendar.google.com', timeMin=now,
+                                          maxResults=results, singleEvents=True,
                                           orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
         print('No upcoming events found.')
     for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(event['start'].get('dateTime'))
+        #start = datetime.datetime(event['start'].get('dateTime')) #, event['start'].get('date'))
+
+        end = event['end'].get('dateTime', event['end'].get('date'))
+
+        #duration
+
+        # 2019-06-01T12:45:00+01:00
+
+
+
         summary = event['summary']
         location = event["location"]
         try:
             description = event["description"]
         except KeyError:
-            print(start, event['summary'], event["location"])
+            print("start", event['summary'], event["location"])
         else:
-            print(start, event['summary'], event["location"], event["description"])
+            print("start", event['summary'], event["location"], event["description"])
 
 
 if __name__ == '__main__':
-    main()
+    initiate()
+    getrecentevents(5)
