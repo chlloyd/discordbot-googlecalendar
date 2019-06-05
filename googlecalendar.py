d8b7f4dc81d9c15b8d236ca114284566fdbaabde
unknown <chris.lloyd2504@gmail.com>
2019-06-05 20:54:33 +0100

    Fixed requirements.txt and datetime in googlecalendar.py

--- googlecalendar.py
+++ googlecalendar.py
@@ -4,6 +4,7 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from dateutil.parser import parse as dtparse
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
@@ -27,31 +28,38 @@ def initiate():
     service = build('calendar', 'v3', credentials=creds)
     return service
 
+
 def getrecentevents(results):
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     print('Getting the upcoming %i events' % results)
-    events_result = initiate().events().list(calendarId='bolkubtob5hn0q1i9btftud944@group.calendar.google.com', timeMin=now,
-                                          maxResults=results, singleEvents=True,
-                                          orderBy='startTime').execute()
+    events_result = initiate().events().list(calendarId='bolkubtob5hn0q1i9btftud944@group.calendar.google.com',
+                                             timeMin=now,
+                                             maxResults=results, singleEvents=True,
+                                             orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
         print('No upcoming events found.')
     for event in events:
-        print(event['start'].get('dateTime'))
-        #start = datetime.datetime(event['start'].get('dateTime')) #, event['start'].get('date'))
+        start = event['start'].get('dateTime', event['start'].get('date'))
+
+        date = datetime.datetime.strftime(dtparse(start), format='%d %B %Y')
+        time = datetime.datetime.strftime(dtparse(start), format='%H:%M')
+
+        print(date, time)
 
         end = event['end'].get('dateTime', event['end'].get('date'))
 
-        #duration
+        # duration
 
         # 2019-06-01T12:45:00+01:00
 
-
-
         summary = event['summary']
         location = event["location"]
+
+        appointment = []
+        print(appointment)
         try:
             description = event["description"]
         except KeyError:
